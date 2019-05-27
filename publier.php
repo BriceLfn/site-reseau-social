48d9fa178cd1c91d3b6182343cc155c6ebfd671c
Yann <yann.dumoulie1@gmail.com>
2019-05-27 11:50:09 +0200

    creation css modif php

--- publier.php
+++ publier.php
@@ -10,18 +10,18 @@ session_start();
 
 <!-- on fait pointer le formulaire vers la page traitant les données -->
 <form action="publier1.php" method="post">
-<table>
-<tr><td>
+
+
 Auteur :
-</td><td>
+
 <input type="text" name="Auteur" maxlength="30" size="50">
-</td></tr><tr><td>
+
 Texte :
-</td><td>
+
 <textarea name="Publication" cols="50" rows="10"></textarea>
-</td></tr><tr><td><td align="right">
+
 <input type="submit" name="go" value="Poster la publication">
-</td></tr></table>
+
 </form>
 <?php
 // on affiche les erreurs éventuelles
