9b6c67b6c0dff6b2c77a08b05e90ca2461eb3999
Yann <yann.dumoulie1@gmail.com>
2019-05-27 14:58:53 +0200

    fond css news
    mise en page

--- news.php
+++ news.php
@@ -1,7 +1,7 @@
 <?php
 // on se connecte à notre base
 try{
-$bdd = new PDO('mysql:host=localhost;dbname=reseausocial;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=localhost;dbname=reseausocial;charset=utf8', 'root', 'root');
 }
 catch(Exception $e)
 {
@@ -16,10 +16,23 @@ $sql = $bdd->query('SELECT * FROM Publication ORDER BY DatePublication DESC');
 
 while($donnees = $sql->fetch()){
 	?>
-	<div style="border : solid 1px">
-	<p> auteur : <?php echo $donnees['Auteur'];?>, publié le <?php echo $donnees['DatePublication'] ?></p>
-	<p>texte : <?php echo $donnees['TextePublication'];?></p>
-	</div>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="publier.css">
+		<title></title>
+	</head>
+	<body>
+		<div class="publication">
+		<p> auteur : <?php echo $donnees['Auteur'];?>, publié le <?php echo $donnees['DatePublication'] ?></p>
+		<p>texte : <?php echo $donnees['TextePublication'];?></p>
+		</div>
+		<div id="particles-js"></div>
+		<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+		<script  src="particles.js"></script>
+	</body>
+	</html>
 <?php
 }
 
